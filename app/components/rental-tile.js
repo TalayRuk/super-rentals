f53121819f3d2c3585d34f6cc6183749bcddc453
vp <notejde@gmail.com>
2016-10-25 13:33:37 -0700

    add {{action 'imageShow'}} to button in rental-tile.js

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isImageShowing: false
+  isImageShowing: false, //we set this as false so it'll always be hidden by default untill the user clicks the button. 
+  //add function to trigger button
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    }
+    //When 'show image' button in rental-tile.hbs is clicked, the imageShow action defined here will run, setting the "isImageShowing" value to true.
+
+  }
 });
