426a3e013e787d8f64420c02aeddb13a640b20ff
vp <notejde@gmail.com>
2016-10-25 15:46:26 -0700

    add delete to routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -22,7 +22,7 @@ import Ember from 'ember';
 //   type: "Apartment",
 //   bedrooms: 3,
 //   image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
-// }];
+// }]; removed this section since we already put it in our rental.json to firebase
 
 export default Ember.Route.extend({
   //add model hook
@@ -31,6 +31,17 @@ export default Ember.Route.extend({
     //to return to the application
     //this.store mean firebase data store.
     //findAll() method w/ rental as argument
+
   },
+  //now we need to add delete actions we created it in rental-tile.hbs & .js & defined destroyRental(rental)
+  actions: {
+    destroyRental(rental) {
+      //When we clicked a rental's delete button, our action up from components(templates/components/rental-tile.hbs to rental-tile.js), through template (templates/index.hbs), & into the route handler(routes/index.js).
+      rental.destroyRecord();
+      //Then the route handler will access our data store (firebase) & delete the obj.
+      this.transitionTo('index');
+      //To return to the templates/index.hbs page after a rental is deleted.
+    }
+  }
 });
 //your table names in Firebase will be a plural model name, and the model hooks in your routes will refer to the singular model name.
