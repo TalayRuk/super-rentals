7c2ed419c6d1b4f23493e88f2d8efa5c52365f54
vp <notejde@gmail.com>
2016-10-26 15:45:02 -0700

    add new route/rental.js

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('rental');
 });
 
 export default Router;//Route & Router is the same the diff is that Route is like a noun & Router is like a verb
