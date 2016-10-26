93a9cc2b2a93f223df30579af1d245e5cacc4c23
vp <notejde@gmail.com>
2016-10-25 17:10:28 -0700

    add function to new-rental.js to show the form

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //set addNewRental as false, then add actions when click to show form
+  addNewRental: false,
+  actions: {
+    //when btn is clicked, action rentalFormShow() set addNewRental value to true to show form.
+    rentalFormShow() {
+      this.set('addNewRental', true);
+    }
+    //Like hide/show image. We'll toggle what's displayed using addNewRental property. 
+  }
 });
