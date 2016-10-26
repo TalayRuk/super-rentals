c636d043cb3b04c24ac6a29e488fd8614985bbe1
vp <notejde@gmail.com>
2016-10-26 11:08:25 -0700

    add new-rental.js

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -8,6 +8,32 @@ export default Ember.Component.extend({
     rentalFormShow() {
       this.set('addNewRental', true);
     }
-    //Like hide/show image. We'll toggle what's displayed using addNewRental property. 
+    //Like hide/show image. We'll toggle what's displayed using addNewRental property.
+
+    //add Save function
+    saveRental1() {
+      //create variable
+      var params = { //like parameter to getter in #C to get info
+        owner: this.get('owner'),
+        type: this.get('type'),
+        city: this.get('city'),
+        bedrooms: this.get('bedrooms'),
+        image: this.get('image'),
+        //When the field is not populated, its value will be undefined
+        //**NOTE undefined is not a valid JSON type. It'll prevent the record from being written to Firebase & cause an error.
+        //For field that might be leave blank or undefined should add a conditional that sets them to "" or null;
+        //example; this.get('owner'): "",
+      };
+      this.set('addNewRental', false);
+      //after each field's value is collected, the form is hidden again)
+      this.sendAction('saveRental2', params);
+      //params in also build in Ember
+    }
+
   }
 });
+//saveRental1 = the action that collects & save data from input in form at new-rental.hbs
+//saveRental2 = a path that send the action up to route handler route/index.js
+
+//**NOTE* saveRental1 collects data & sets them as values in a hash of keys w/ the same names.
+//this.get('owner') collects the value entered in the owner input field & assigns it to the variable key owner
