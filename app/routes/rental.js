b6baa8587cc511b2c32eb86d0a5a658ec0b401c3
vp <notejde@gmail.com>
2016-10-26 16:32:13 -0700

    add find Record w/Dynamic Segment(id) routes/rental.js

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
-
+//Finding Records w/Dynamic Segment Info
+//The rental route handler will then use the params.rental_id in the model hook to locate the single rental record we've requested:
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('rental', params.rental_id);
+    //findRecord() using id which takes 2 arguments; 1) the type of object('rental') 2) the object's specific id(params.rental_id)
+    //After specified rental, the route will render the rental template/rental.hbs 
+  },
 });
