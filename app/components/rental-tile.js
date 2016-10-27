d7d23d727b67d5c547309625759710684479ec81
vp <notejde@gmail.com>
2016-10-26 21:15:26 -0700

    add removed update() & delete() from rental-tile.js since now we'll have it in rental.js instead

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -4,7 +4,9 @@ export default Ember.Component.extend({
   isImageShowing: false, //we set this as false so it'll always be hidden by default untill the user clicks the button.
   //add function to trigger button
   //also need to add UPDATE FUNCTION***
-  updateRentalForm: false,
+  //******REMOVED b/c UPDATE() ACTION IS NOW UPDATING ON THE RENTAL TEMPLATES/RENTAL.HBS*************
+  // updateRentalForm: false,
+//************************************
 
   actions: {
     imageShow: function() {
@@ -15,20 +17,21 @@ export default Ember.Component.extend({
     imageHide: function() {
       this.set('isImageShowing', false);
       //set back to false so the image will hide when it's clicked.
-    },
+    }
+    //************REMOVED UPDATE()****************************
     //add UPDATE function w/update(), rental argument & params
-    update(rental, params) {
-      //Add sendAction to pass up all 3 properties to route handler
-      this.sendAction('update', rental, params);
-    //update() action in rental-tile is triggered when the child compone(update-rental) calls this.sendAction ..  which this actin just pass along all 3 properties up to index
-    //***Now we need to add update="update" action btwn rental=rental & destroyRental="destroyRental" in index route ... templates/index.hbs then we'll add update() in app/route/index.js
-    },
-
-    delete(rental) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
-    }  //When delete clicked, the delete action will display a confirmation pop-up. Then If confirmed, it'll send the action destroyRental() upwards into Route handler by calling this.Action() (from rental-tile.hbs to rental-tile.js --> templates/index.hbs -> to routes/index.js --> to MODEL(firebase) not w/in the component!!!)
+    // update(rental, params) {
+    //   //Add sendAction to pass up all 3 properties to route handler
+    //   this.sendAction('update', rental, params);
+    // //update() action in rental-tile is triggered when the child compone(update-rental) calls this.sendAction ..  which this actin just pass along all 3 properties up to index
+    // //***Now we need to add update="update" action btwn rental=rental & destroyRental="destroyRental" in index route ... templates/index.hbs then we'll add update() in app/route/index.js
+    // },
+    //
+    // delete(rental) {
+    //   if (confirm('Are you sure you want to delete this rental?')) {
+    //     this.sendAction('destroyRental', rental);
+    //   }
+    // }  //When delete clicked, the delete action will display a confirmation pop-up. Then If confirmed, it'll send the action destroyRental() upwards into Route handler by calling this.Action() (from rental-tile.hbs to rental-tile.js --> templates/index.hbs -> to routes/index.js --> to MODEL(firebase) not w/in the component!!!)
 
       //this.sendAction need argument that we provide the name of the function on the template & route handler we'd like to activate (in this case, destroyRental(), that we'll create in index.hbs) & the specific rental to provide that action.
   }
