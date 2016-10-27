cda493fdaf7f55c0a753a31293a8ddb7d014e0f1
vp <notejde@gmail.com>
2016-10-27 15:11:55 -0700

    add update to rental.hbs & index.hbs for new-review

--- app/models/review.js
+++ app/models/review.js
@@ -5,4 +5,9 @@ export default DS.Model.extend({
   rating: DS.attr(),
   content: DS.attr()
   rentals: DS.belongsTo('rental', { async: true}),
+  //add rental attribute to the review model to refer of our rental objects.
 });
+
+//after add rental attribute. We'll modify rental.hbs so that when a review is added it'll be stored in a rental's reviews attribute and rental also will be stored in the review's rental attribute.
+//we also need to change the review to be accessed from inside the specific rental instead of from the index.
+//**Thus, we'll move {{new-review}} from index to rental.hbs
