a7d59c2e06e7ea36361ce8ff9678e018d80070bc
vp <notejde@gmail.com>
2016-10-26 21:08:57 -0700

    add removed update() & destroyRental() from index.js to rental.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -41,36 +41,37 @@ export default Ember.Route.extend({
       this.transitionTo('index');
       //Then transition back to index page & show new rental added to the list.
     },
-
-    update(rental, params) {
-      // use - this.store.update('update', rental, params) but since this is the update()='update'(this is the update() that came up from rental-tile.hbs from .js from update-rental.js)
-      //ADD Debugger from chrome extension "Ember Inspector"
-      debugger;
-      //add update() code
-      Object.keys(params).forEach(function(key) {
-        //for each key in params,
-        if(params[key]!==undefined) {
-          //if it's not undefined
-          rental.set(key,params[key]);
-          //take the rental & set the property that matches the current key to the value of the current key,
-        }
-        //this assures that properties the user has updated will be changed accordingly, but properties the user has not updated will remain the same(instead of being redefined as undefined) B/c they didn't want to change those.
-      });
-      //after looping through all keys
-      // save the rental,
-      rental.save();
-      //then transition to the index route. to reload index w/updated 
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental) {
-      //When we clicked a rental's delete button, our action up from components(templates/components/rental-tile.hbs to rental-tile.js), through template (templates/index.hbs), & into the route handler(routes/index.js).
-      rental.destroyRecord();
-      //Then the route handler will access our data store (firebase) & delete the obj.
-      //**destroyRecord() is already exist in Ember Method
-      this.transitionTo('index');
-      //To return to the templates/index.hbs page after a rental is deleted.
-    }
+//******MOVED TO App/Routes/RENTAL.JS
+    // update(rental, params) {
+    //   // use - this.store.update('update', rental, params) but since this is the update()='update'(this is the update() that came up from rental-tile.hbs from .js from update-rental.js)
+    //   //ADD Debugger from chrome extension "Ember Inspector"
+    //   debugger;
+    //   //add update() code
+    //   Object.keys(params).forEach(function(key) {
+    //     //for each key in params,
+    //     if(params[key]!==undefined) {
+    //       //if it's not undefined
+    //       rental.set(key,params[key]);
+    //       //take the rental & set the property that matches the current key to the value of the current key,
+    //     }
+    //     //this assures that properties the user has updated will be changed accordingly, but properties the user has not updated will remain the same(instead of being redefined as undefined) B/c they didn't want to change those.
+    //   });
+    //   //after looping through all keys
+    //   // save the rental,
+    //   rental.save();
+    //   //then transition to the index route. to reload index w/updated
+    //   this.transitionTo('index');
+    // },
+    //
+    // destroyRental(rental) {
+    //   //When we clicked a rental's delete button, our action up from components(templates/components/rental-tile.hbs to rental-tile.js), through template (templates/index.hbs), & into the route handler(routes/index.js).
+    //   rental.destroyRecord();
+    //   //Then the route handler will access our data store (firebase) & delete the obj.
+    //   //**destroyRecord() is already exist in Ember Method
+    //   this.transitionTo('index');
+    //   //To return to the templates/index.hbs page after a rental is deleted.
+    // }
+    //**************END OF SECTION MOVED TO RENTAL.JS***************
     //check out: http://emberjs.com/api/classes/Ember.Route.html Ember.Route Class ..methods, properties, events
   }
 });
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -6,6 +6,35 @@ export default Ember.Route.extend({
     return this.store.findRecord('rental', params.rental_id);
     //findRecord() using id which takes 2 arguments; 1) the type of object('rental') 2) the object's specific id(params.rental_id)
     //After specified rental, the route will render the rental template/rental.hbs
-    
+
   },
+  actions: {//moved from index.js
+    update(rental, params) {
+      // use - this.store.update('update', rental, params) but since this is the update()='update'(this is the update() that came up from rental-tile.hbs from .js from update-rental.js)
+      //ADD Debugger from chrome extension "Ember Inspector"
+      debugger;
+      //add update() code
+      Object.keys(params).forEach(function(key) {
+        //for each key in params,
+        if(params[key]!==undefined) {
+          //if it's not undefined
+          rental.set(key,params[key]);
+          //take the rental & set the property that matches the current key to the value of the current key,
+        }
+        //this assures that properties the user has updated will be changed accordingly, but properties the user has not updated will remain the same(instead of being redefined as undefined) B/c they didn't want to change those.
+      });
+      //after looping through all keys
+      // save the rental,
+      rental.save();
+      //then transition to the index route. to reload index w/updated
+      this.transitionTo('index');
+    },
+    destroyRental(rental) {
+      //When we clicked a rental's delete button, our action up from components(templates/components/rental-tile.hbs to rental-tile.js), through template (templates/index.hbs), & into the route handler(routes/index.js).
+      rental.destroyRecord();
+      //Then the route handler will access our data store (firebase) & delete the obj.
+      //**destroyRecord() is already exist in Ember Method
+      this.transitionTo('index');
+      //To return to the templates/index.hbs page after a rental is deleted.
+    }
 });
