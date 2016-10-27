aec92699b61fa1d36c0f55642ecc2df8e18f5cab
vp <notejde@gmail.com>
2016-10-27 12:08:28 -0700

    add new-review.js

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //if addNewReview is false
+  addNewReview: false,
+
+  actions: {
+    //set.addNewReview is true, tell form to show
+    showReviewForm(){
+      this.set('addNewReview', true);
+    },
+    //'saveReview' model.reviews.Save() by set up variable
+    saveReview() {
+      var params = {//add properties
+        author: this.get('author'),
+        rate: this.get('rate'),
+        content: this.get('content')
+      };
+      //now we have to set addNewReview to false again to hide
+      this.set('addNewReview', false);
+      this.sendAction('saveReview', params); //actions up to route handler = routes/index.js
+    }
+  }
 });
