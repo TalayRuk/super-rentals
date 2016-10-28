83dd685e19f47b8ffd120673f370e752db3b523d
vp <notejde@gmail.com>
2016-10-27 18:40:26 -0700

    add fixed bug now functioning

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   actions: {
     delete(review) {
       if (confirm('Are you sure you want to delete this review?')) {
-        this.sendAction('destroyReview' review);
+        this.sendAction('destroyReview', review);
       }
     }
   }
